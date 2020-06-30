f5cbeeb1a243c867b916909113e749fccffd18a1
SQL47 <ajidalmajid6@gmail.com>
2020-07-01 06:42:32 +0700

    update : detail vendor

--- admin/dashboard/verifikasi-vendor/index.php
+++ admin/dashboard/verifikasi-vendor/index.php
@@ -37,11 +37,40 @@
 																		<td><?= $vendor['alamat']; ?></td>
 																		<td><?= $vendor['telp']; ?></td>
 																		<td>
-																			<button data-id="<?= $vendor['id_vendor']; ?>" class="btn btn-info btn-xs">Detail</button>
+																			<button class="btn btn-info btn-xs" data-toggle="modal" data-target="#detailVendor<?= $vendor['id_vendor']; ?>">Detail</button>
 																			<a href="verifikasi-vendor/terima.php?id=<?= $vendor['id_vendor']; ?>" class="btn btn-warning btn-xs">Terima</a>
 																			<a href="verifikasi-vendor/tolak.php?id=<?= $vendor['id_vendor']; ?>" class="btn btn-danger btn-xs">Tolak</a>
 																		</td>
 																</tr>
+
+																<div class="modal fade" id="detailVendor<?= $vendor['id_vendor']; ?>" tabindex="-1" role="dialog"
+                                    aria-labelledby="scrollableModalTitle" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-scrollable" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="scrollableModalTitle">Detail Vendor</h5>
+                                                <button type="button" class="close" data-dismiss="modal"
+                                                    aria-label="Close">
+                                                    <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+																								<p>Username : <?= $vendor['username']; ?></p>
+                                                <p>Nama Vendor : <?= $vendor['nama_vendor']; ?></p>
+                                                <p>Pemilik : <?= $vendor['nama_pemilik']; ?></p>
+                                                <p>KTP: <img src="../img/<?= $vendor['ktp']; ?>" style="width:50%"></p>
+                                                <p>Alamat : <?= $vendor['alamat']; ?></p>
+                                                <p>No. Telp : <?= $vendor['telp']; ?></p>
+																								<p>Tgl daftar: <?= $vendor['tgl_daftar']; ?></p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-dismiss="modal">Close</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
 															<?php endforeach; ?>
 														</tbody>
 												</table>
